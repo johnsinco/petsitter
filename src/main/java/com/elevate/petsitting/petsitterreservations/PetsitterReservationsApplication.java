4202763b1593ca8b17dc02ccfdb135273ff7b58e
John Stewart <john@Johns-MacBook-Pro.local>
2018-05-19 15:54:53 -0600

    spring config

---
+++ src/main/java/com/elevate/petsitting/petsitterreservations/PetsitterReservationsApplication.java
@@ -0,0 +1,12 @@
+package com.elevate.petsitting.petsitterreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsitterReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsitterReservationsApplication.class, args);
+	}
+}
